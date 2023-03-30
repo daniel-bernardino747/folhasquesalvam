18b4b93871f1dd80006fd89261367209742c217a
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2023-03-30 11:48:46 -0300

    feat: Add dependencies and Swagger support to back-end - Add @clerk/clerk-sdk-node, dotenv - Add  @nestjs/swagger, swagger-ui-express - Configure Swagger for API

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Folhas Que Salvam')
+    .setDescription(
+      `🌿 Folhas Que Salvam is an environmental project aimed at raising awareness about the importance of preserving nature and promoting sustainable practices. 
+      This full-stack project 🚀 utilizes a combination of cutting-edge technologies such as Next.js, Clerk, and Nest.js to deliver a seamless user experience. 🌍`,
+    )
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(4000);
 }
 bootstrap();
