2af396836df203925b8ccb26c7521e218c0d0081
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2023-03-31 09:12:13 -0300

    feat: Add seed data for e2e tests and config for dotenv in back-end

---
+++ back-end/test/e2e/member.e2e-spec.ts
@@ -0,0 +1,35 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../../src/app.module';
+
+describe('MemberController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  describe('GET /api/health', () => {
+    it('returns a "OK" message when the health endpoint is called', async () => {
+      const { status, body } = await request(app.getHttpServer()).get(
+        '/api/health',
+      );
+
+      expect(status).toBe(200);
+      expect(body.message).toBe('OK');
+
+      // 1 second timeout for request response
+      const timeout = setTimeout(() => {
+        expect(body.uptime).toBeCloseTo(process.uptime(), 1);
+      }, 1000);
+      await new Promise((resolve) => setTimeout(resolve, 500));
+      clearTimeout(timeout);
+    });
+  });
+});
