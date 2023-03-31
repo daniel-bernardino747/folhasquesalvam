41a425d20732555aa267c22c50773ad0fa0abdfd
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2023-03-31 09:11:31 -0300

    feat: setup main and app module - remove AppService import - add ValidationPipe - await Roles Guard

--- back-end/src/app.controller.spec.ts
+++ back-end/src/app.controller.spec.ts
@@ -1,6 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -8,15 +7,14 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
     }).compile();
 
     appController = app.get<AppController>(AppController);
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return a "OK" message when the health endpoint is called', () => {
+      expect(appController.getHealth().message).toBe('OK');
     });
   });
 });
--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+// import { APP_GUARD } from '@nestjs/core';
 
-import { AppController } from './controllers/app.controller';
+import { AppController } from './app.controller';
 
+import { MembersModule } from './members/members.module';
+import { UsersModule } from './users/users.module';
+// import { RolesGuard } from './roles.guard';
 @Module({
+  imports: [MembersModule, UsersModule],
   controllers: [AppController],
+  // providers: [{ provide: APP_GUARD, useClass: RolesGuard }],
 })
 export class AppModule {}
