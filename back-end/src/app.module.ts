e19209a472ff75c7fb7152889ce77a1fda30f24b
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2023-03-31 09:25:37 -0300

    feat: add CRUD, dtos, entities of Goals and Points

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -5,9 +5,12 @@ import { AppController } from './app.controller';
 
 import { MembersModule } from './members/members.module';
 import { UsersModule } from './users/users.module';
+import { GoalsModule } from './goals/goals.module';
+import { PointsModule } from './points/points.module';
+
 // import { RolesGuard } from './roles.guard';
 @Module({
-  imports: [MembersModule, UsersModule],
+  imports: [MembersModule, UsersModule, GoalsModule, PointsModule],
   controllers: [AppController],
   // providers: [{ provide: APP_GUARD, useClass: RolesGuard }],
 })
