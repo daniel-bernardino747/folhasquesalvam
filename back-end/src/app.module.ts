1bdef7a2b20942e8ebf1bfef0687b7ab34eed396
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2023-04-05 20:14:43 -0300

    feat: Add AuthGuard and AuthModule to AppModule

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-// import { APP_GUARD } from '@nestjs/core';
+import { APP_GUARD } from '@nestjs/core';
 
 import { AppController } from './app.controller';
 
@@ -8,10 +8,16 @@ import { UsersModule } from './users/users.module';
 import { GoalsModule } from './goals/goals.module';
 import { PointsModule } from './points/points.module';
 
-// import { RolesGuard } from './roles.guard';
+import { RolesGuard } from './roles.guard';
+import { AuthGuard } from './auth/auth.guard';
+import { AuthModule } from './auth/auth.module';
+
 @Module({
-  imports: [MembersModule, UsersModule, GoalsModule, PointsModule],
+  imports: [MembersModule, UsersModule, GoalsModule, PointsModule, AuthModule],
   controllers: [AppController],
-  // providers: [{ provide: APP_GUARD, useClass: RolesGuard }],
+  providers: [
+    { provide: APP_GUARD, useClass: AuthGuard },
+    { provide: APP_GUARD, useClass: RolesGuard },
+  ],
 })
 export class AppModule {}
