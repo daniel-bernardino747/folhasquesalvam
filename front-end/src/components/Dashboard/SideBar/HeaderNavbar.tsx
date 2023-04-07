ee02588f17818fe1a83f53bc711fba4cbf9c253d
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2023-04-07 17:29:21 -0300

    refactor: add and separe HeaderSidebar - add clsx for conditionals classNames

---
+++ front-end/src/components/Dashboard/SideBar/HeaderNavbar.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import Logo from "../../../../public/folhas-logo.png";
+import { AiOutlineDoubleLeft } from "react-icons/ai";
+import clsx from "clsx";
+
+type HeaderNavbarProps = {
+  collapseSidebar: (collapsed?: boolean | undefined) => void;
+  collapsed: boolean;
+};
+
+export const HeaderNavbar = ({
+  collapseSidebar,
+  collapsed,
+}: HeaderNavbarProps) => (
+  <div className="flex h-16 items-center justify-between border-b-[1px] border-[#DBDBDB] px-5 py-4">
+    <div className="flex items-center hover:cursor-pointer">
+      <Image
+        onClick={() => collapseSidebar()}
+        src={Logo}
+        width={30}
+        height={30}
+        alt="main-logo-folhas"
+      />
+      <p
+        className={clsx(
+          "border-[#DBDBDB] pl-2 pt-1 text-lg font-bold text-[#0D062D]",
+          { hidden: collapsed }
+        )}
+      >
+        Folhas
+      </p>
+    </div>
+    <AiOutlineDoubleLeft
+      className={clsx("border-[#DBDBDB] hover:cursor-pointer", {
+        hidden: collapsed,
+      })}
+      fill="#787486"
+      onClick={() => collapseSidebar()}
+    />
+  </div>
+);
--- front-end/src/components/Dashboard/SideBar/index.tsx
+++ front-end/src/components/Dashboard/SideBar/index.tsx
@@ -1,91 +1,35 @@
-import Image from "next/image";
-import Logo from "../../../../public/folhas-logo.png";
+import { usePathname } from "next/navigation";
 import * as pro from "react-pro-sidebar";
-import { AiOutlineDoubleLeft, AiOutlineSetting } from "react-icons/ai";
+import clsx from "clsx";
+
+import { HeaderNavbar } from "./HeaderNavbar";
+
+import { AiOutlineSetting } from "react-icons/ai";
 import { BsGrid, BsChatDots, BsUiChecksGrid } from "react-icons/bs";
 import { HiOutlineUsers } from "react-icons/hi";
 
-export function SideBar() {
-  const { collapseSidebar, collapsed } = pro.useProSidebar();
-
-  return (
-    <pro.Sidebar
-      rootStyles={configStyleSideBar}
-      collapsedWidth="70px"
-      width="200px"
-    >
-      <div className="flex h-16 items-center justify-between border-b-[1px] border-[#DBDBDB] px-5 py-4">
-        <div className="flex items-center hover:cursor-pointer">
-          <Image
-            onClick={() => collapseSidebar()}
-            src={Logo}
-            width={30}
-            height={30}
-            alt="main-logo-folhas"
-          />
-          <p
-            className={
-              collapsed
-                ? "hidden"
-                : "none border-[#DBDBDB] pl-2 pt-1 text-lg font-bold text-[#0D062D]"
-            }
-          >
-            Folhas
-          </p>
-        </div>
-        <AiOutlineDoubleLeft
-          className={
-            collapsed ? "hidden" : "none border-[#DBDBDB] hover:cursor-pointer"
-          }
-          fill="#787486"
-          onClick={() => collapseSidebar()}
-        />
-      </div>
-      <pro.Menu>
-        <pro.MenuItem>
-          <div
-            className={collapsed ? "flex justify-center" : "flex items-center"}
-          >
-            <BsGrid />
-            <p className={collapsed ? "hidden" : "none pl-2"}> Home </p>
-          </div>
-        </pro.MenuItem>
-        <pro.MenuItem>
-          <div
-            className={collapsed ? "flex justify-center" : "flex items-center"}
-          >
-            <BsChatDots />
-            <p className={collapsed ? "hidden" : "none pl-2"}> Avisos </p>
-          </div>
-        </pro.MenuItem>
-        <pro.MenuItem>
-          <div
-            className={collapsed ? "flex justify-center" : "flex items-center"}
-          >
-            <BsUiChecksGrid />
-            <p className={collapsed ? "hidden" : "none pl-2"}> Metas </p>
-          </div>
-        </pro.MenuItem>
-        <pro.MenuItem>
-          <div
-            className={collapsed ? "flex justify-center" : "flex items-center"}
-          >
-            <HiOutlineUsers />
-            <p className={collapsed ? "hidden" : "none pl-2"}> Membros </p>
-          </div>
-        </pro.MenuItem>
-        <pro.MenuItem>
-          <div
-            className={collapsed ? "flex justify-center" : "flex items-center"}
-          >
-            <AiOutlineSetting />
-            <p className={collapsed ? "hidden" : "none pl-2"}> Configuração </p>
-          </div>
-        </pro.MenuItem>
-      </pro.Menu>
-    </pro.Sidebar>
-  );
-}
+const navItems = {
+  "/dashboard": {
+    name: "Dashboard",
+    icon: <BsGrid />,
+  },
+  "/messages": {
+    name: "Avisos",
+    icon: <BsChatDots />,
+  },
+  "/goals": {
+    name: "Metas",
+    icon: <BsUiChecksGrid />,
+  },
+  "/members": {
+    name: "Membros",
+    icon: <HiOutlineUsers />,
+  },
+  "/settings": {
+    name: "Configuração",
+    icon: <AiOutlineSetting />,
+  },
+};
 
 const configStyleSideBar = {
   [`.${pro.sidebarClasses.container}`]: {
@@ -108,3 +52,45 @@ const configStyleSideBar = {
     },
   },
 };
+
+export function SideBar() {
+  let pathname = usePathname() || "/";
+  const { collapseSidebar, collapsed } = pro.useProSidebar();
+
+  return (
+    <pro.Sidebar
+      rootStyles={configStyleSideBar}
+      collapsedWidth="70px"
+      width="200px"
+    >
+      <HeaderNavbar collapseSidebar={collapseSidebar} collapsed={collapsed} />
+      <pro.Menu>
+        {Object.entries(navItems).map(([path, { name, icon }]) => {
+          const isActive = path === pathname;
+          return (
+            <pro.MenuItem key={path}>
+              <div
+                className={clsx("flex", {
+                  "justify-center": collapsed,
+                  "items-center": !collapsed,
+                  "text-black": isActive,
+                  "": !isActive,
+                })}
+              >
+                {icon}
+                <p
+                  className={clsx({
+                    hidden: collapsed,
+                    "none pl-2": !collapsed,
+                  })}
+                >
+                  {name}
+                </p>
+              </div>
+            </pro.MenuItem>
+          );
+        })}
+      </pro.Menu>
+    </pro.Sidebar>
+  );
+}
