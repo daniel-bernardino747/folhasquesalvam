ed3880bc04433ecaedc85614be659324d9eda18d
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2023-04-17 14:29:52 -0300

    fix: translate title page members => membros

--- front-end/src/app/(portal)/members/page.tsx
+++ front-end/src/app/(portal)/members/page.tsx
@@ -5,7 +5,7 @@ export default function MembersPage() {
   return (
     <div className="h-screen">
       <div className="flex justify-between font-normal">
-        <h1 className="text-4xl">Members</h1>
+        <h1 className="text-4xl">Membros</h1>
       </div>
       <MemberList />
     </div>
