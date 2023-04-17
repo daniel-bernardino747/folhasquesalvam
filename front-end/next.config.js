5a0fc87a61f5023b4fe85111878c197c4f650810
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2023-04-17 15:17:30 -0300

    chore: Add image domain to next.config.js

--- front-end/next.config.js
+++ front-end/next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  images: {
+    domains: ["images.clerk.dev"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
